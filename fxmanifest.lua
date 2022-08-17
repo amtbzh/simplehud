5a9a484e4ee366a24e8d408645b9e734fea6ae55
Anthony Morisset <amtbzh@gmail.com>
2022-08-17 18:07:36 +0200

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+-- AMTBZH
+-- Simple HUD 
+-- FOR FREE
+-- You don't need to buy this 3$ or more OMG
+fx_version 'cerulean'
+author 'AMTBZH'
+description 'Simple HUD'
+version '1.0'
+game 'gta5'
+
+lua54 'on'
+
+ui_page {
+    'html/index.html'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+files {
+	'html/index.html'
+}
